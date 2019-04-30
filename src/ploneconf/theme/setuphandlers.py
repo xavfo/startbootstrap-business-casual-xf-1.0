9fb8add923f1b874e1f60405aac7238c5dbc5ea4
xavfo <xavier.freire@gmail.com>
2019-04-30 11:55:26 -0500

    slider images initial content

--- src/ploneconf/theme/setuphandlers.py
+++ src/ploneconf/theme/setuphandlers.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
+import os
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
@@ -16,8 +18,47 @@ class HiddenProfiles(object):
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
+    portal = api.portal.get()
+    _create_content(portal)
 
 
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+def _create_content(portal):
+    if not portal.get('slider-images', False):
+        slider = api.content.create(
+            type='Folder',
+            container=portal,
+            title=u'Slider',
+            id='slider-images'
+        )
+        for slider_number in range(1, 4):
+            slider_name = u'slider-{0}'.format(str(slider_number))
+            slider_image = api.content.create(
+                type='Image',
+                container=slider,
+                title=slider_name,
+                id=slider_name
+            )
+            slider_image.image = _load_image(slider_number)
+        # NOTE: if your plone site is not a vanilla plone
+        # you can have different workflows on folders and images
+        # or different transitions names so this could fail
+        # and you'll need to publish the images as well
+        # or do that manually TTW.
+        api.content.transition(obj=slider, transition='publish')
+
+
+def _load_image(slider):
+    from plone.namedfile.file import NamedBlobImage
+    filename = os.path.join(
+        os.path.dirname(__file__),
+        'theme',
+        'img',
+        'slide-{0}.jpg'.format(slider),
+    )
+    return NamedBlobImage(
+        data=open(filename, 'r').read(),
+        filename=u'slide-{0}.jpg'.format(slider)
+    )
